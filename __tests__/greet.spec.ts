b348c7f00b779542e27cb5bd9e46183047e60682
Gago <xeroice@gmail.com>
2020-10-29 20:56:17 -0700

    clean up

--- __tests__/greet.spec.ts
+++ __tests__/greet.spec.ts
@@ -2,6 +2,6 @@ import { greet } from '../index';
 
 describe('Greet', () => {
   it('should output a greeting', async () => {
-    expect(await greet({ input: 'Bob' })).toMatchInlineSnapshot('Greetings Bob');
+    expect(await greet({ input: 'Bob' })).toMatchInlineSnapshot('"Greetings Bob"');
   });
 });
